d3dde0550ef7f8482256691454709a4036866edc
Make Hehe <herosquid2@gmail.com>
2025-04-06 20:33:28 +0300

    Added buttons to character creator for characteristics and skills

--- main.py
+++ main.py
@@ -8,9 +8,11 @@ import tkinter as tk
 script_dir = os.path.dirname(os.path.abspath(__file__))
 configs_dir = os.path.join(script_dir,"configs")
 settings_file = os.path.join(configs_dir, "Settings")
+charorigin_file = os.path.join(configs_dir,"character_origin")
 items_dir = os.path.join(script_dir,"items")
 spells_dir = os.path.join(script_dir,"spells")
 characters_dir = os.path.join(script_dir,"characters")
+feature_dir = os.path.join(script_dir,"features")
 
 
 class Config:
@@ -32,6 +34,7 @@ class Config:
             self.settings_name.append(content[0])
             self.settings_value.append(content[1])
         print(self.settings_name,self.settings_value)
+
     def save(self, list):
         X = 0
         lines = []
@@ -69,8 +72,10 @@ class MainWindow(tk.Tk):
     def __init__(self, screenName = None, baseName = None, className = "Tk", useTk = True, sync = False, use = None):
         super().__init__(screenName, baseName, className, useTk, sync, use)
         self.config = Config()
-
-        self.geometry(self.config.geometry)
+        try:
+            self.geometry(self.config.geometry)
+        except:
+            self.geometry("500x500")
         self.title(self.config.title)
         self.buttons = []
         self.GUI_init()
@@ -91,13 +96,159 @@ class MainWindow(tk.Tk):
         b_decline.pack(side="right",padx=3)
         self.buttons.append(b_decline)
 
-    def character_creator(self):
+    def charcreator_change_val(self, target, value, cplabel, splabel): # Character creator +1 -1 button activation
+
+        # Extract values
+        cpval = cplabel.cget("text")
+        cpcontent = cpval.split(": ")
+        current_cp = int(cpcontent[1])
+        spval = splabel.cget("text")
+        spcontent = spval.split(": ")
+        current_sp = int(spcontent[1])
+        targetval = target.cget("text")
+        targetcontent = targetval.split(": ")
+        currentval = int(targetcontent[1])
+
+        # Check values
+        print(f"Current value {currentval}, SP remaining {current_sp}, CP remaining {current_cp}")
+        print(targetcontent[0])
+
+        # Check type
+        targetskill = targetcontent[0]
+        if targetskill == "Body" or targetskill == "Mind" or targetskill == "Soul":
+            if current_cp > 0 and value == "+1":
+                currentval += 1
+                current_cp -= 1
+                targettext = f"{targetcontent[0]}: {str(currentval)}"
+                target.config(text=targettext)
+                cptext = f"Characteristic Points Remaining: {str(current_cp)}"
+                cplabel.config(text=cptext)
+            elif currentval > 0 and value == "-1":
+                currentval -= 1
+                current_cp += 1
+                targettext = f"{targetcontent[0]}: {str(currentval)}"
+                target.config(text=targettext)
+                cptext = f"Characteristic Points Remaining: {str(current_cp)}"
+                cplabel.config(text=cptext)
+
+        else:
+            if current_sp > 0 and value == "+1":
+                currentval += 1
+                current_sp -= 1
+                targettext = f"{targetcontent[0]}: {str(currentval)}"
+                target.config(text=targettext)
+                sptext = f"Characteristic Points Remaining: {str(current_sp)}"
+                splabel.config(text=sptext)
+            elif currentval > 0 and value == "-1":
+                currentval -= 1
+                current_sp += 1
+                targettext = f"{targetcontent[0]}: {str(currentval)}"
+                target.config(text=targettext)
+                sptext = f"Characteristic Points Remaining: {str(current_sp)}"
+                splabel.config(text=sptext)
+
+    def character_creator(self): # Enter character creator
         for button in self.buttons:
             button.destroy()
+
+        lines = []
+        with open(charorigin_file, "r") as file:
+            for line in file:
+                if line != "\n":
+                    line = line.strip()
+                    lines.append(str(line))
+        
+        for line in lines:
+            print(line)
+        print(lines)
+
+
+
+        label = tk.Label(self,text="Character Creator", font=30)
+        label.pack(side="top", pady=5)
+        self.buttons.append(label)
+
+        pointframe = tk.Frame(self)
+        pointframe.pack(side="top")
+        self.buttons.append(pointframe)
+
+
+        CPlabel = tk.Label(pointframe, text="Characteristic Points Remaining: 2")
+        CPlabel.pack(side="left")
+        self.buttons.append(CPlabel)
+
+        SPlabel = tk.Label(pointframe, text = "Skill Points remaining: 5")
+        SPlabel.pack(side="right")
+        self.buttons.append(SPlabel)
+
+        mainframe = tk.Frame(self)
+        mainframe.pack(side="top",pady=5)
+        self.buttons.append(mainframe)
+
+        charframe = None
+        colors = ["red", "light blue", "yellow"]
+        color = None
+        
+
+        for line in lines:
+            content = line.split("=")
+            # frames for the three characteristics
+            if content[0] == "name":
+                continue
+            if content[0] == "body" or content[0] == "mind" or content[0] == "soul":
+                charframe = tk.Frame(mainframe)
+                charframe.pack(side="left",padx=15)
+                self.buttons.append(charframe)
+            #buttons for characteristics and their skills
+            if content[0] == "body" or content[0] == "mind" or content[0] == "soul":
+                color = colors[0]
+                colors.remove(color)
+                char = content[0].capitalize()
+                val = content[1]
+
+                buttonframe = tk.Frame(charframe)
+                buttonframe.pack(side="top",pady=5)
+                self.buttons.append(buttonframe)
+
+                mainbutton = tk.Button(buttonframe,  text=f"{char}: {val}",width=10, bg=color, font=("Arial Black", 8))
+                mainbutton.pack(anchor="center")
+                self.buttons.append(mainbutton)
+
+                redu_button = tk.Button(buttonframe, text="-1", command=lambda b=mainbutton: self.charcreator_change_val(b, "-1",CPlabel, SPlabel),width=8)
+                redu_button.pack(side="left",padx=2)
+                self.buttons.append(redu_button)
+
+                incr_button = tk.Button(buttonframe, text="+1", command=lambda b=mainbutton: self.charcreator_change_val(b, "+1",CPlabel, SPlabel),width=8)
+                incr_button.pack(side="right",padx=2)
+                self.buttons.append(incr_button)
+            else:
+                char = content[0].capitalize()
+                val = content[1]
+
+                buttonframe = tk.Frame(charframe)
+                buttonframe.pack(side="top",pady=2)
+                self.buttons.append(buttonframe)
+
+                mainbutton = tk.Button(buttonframe,  text=f"{char}: {val}",width=12, bg=color)
+                mainbutton.pack(anchor="center")
+                self.buttons.append(mainbutton)
+
+                redu_button = tk.Button(buttonframe, text="-1", command=lambda b=mainbutton: self.charcreator_change_val(b, "-1",CPlabel, SPlabel),width=8)
+                redu_button.pack(side="left",padx=2)
+                self.buttons.append(redu_button)
+
+                incr_button = tk.Button(buttonframe, text="+1", command=lambda b=mainbutton: self.charcreator_change_val(b, "+1",CPlabel, SPlabel),width=8)
+                incr_button.pack(side="right",padx=2)
+                self.buttons.append(incr_button)
+            
+
+        
 
         back_main = tk.Button(self, text="Back", font=10, command=self.GUI_init)
         back_main.pack(side="bottom")
         self.buttons.append(back_main)
+
+    
     
     def GUI_init(self): # MAIN
         for entity in self.buttons:
